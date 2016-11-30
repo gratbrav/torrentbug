363004a162165977f4684e37a34e53171d88b776
gratbrav <spam@huxsohl.de>
2016-11-30 19:42:02 +0100

    rewrite autoloader

--- src/Class/autoload.php
+++ src/Class/autoload.php
@@ -1,18 +1,65 @@
 <?php
+/**
+ * Autoloader
+ *
+ * Autoload required class
+ *
+ * @package  Torrentbug
+ * @author   Gratbrav
+ */
+class Autoloader
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        spl_autoload_register([$this, 'loadClass']);
+    }
 
-	function autoload($className)
-	{
-	    $className = ltrim($className, '\\');
-	    $fileName  = '';
-	    $namespace = '';
-	    if ($lastNsPos = strrpos($className, '\\')) {
-	        $namespace = substr($className, 0, $lastNsPos);
-	        $className = substr($className, $lastNsPos + 1);
-	        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-	    }
-	    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    /**
+     * Create autoloader
+     */
+    public static function register()
+    {
+        new Autoloader();
+    }
 
-	    require __DIR__ . '/../' . $fileName;
-	}
+    /**
+     * Load class
+     * 
+     * @param string $className
+     */
+    public function loadClass($className)
+    {
+        $file = str_replace('\\', '/', $className);
+        $file = __DIR__ . '/' . $file . '.php';
 
-	spl_autoload_register('autoload');
+        if (file_exists($file)) {
+            require_once $file;
+        } else {
+            $this->fallback($className);
+        }
+    }
+
+    /**
+     * Fallback to load class without namespace
+     * 
+     * @param string $className
+     */
+    protected function fallback ($className)
+    {
+        $className = ltrim($className, '\\');
+        $fileName  = '';
+        if ($lastNsPos = strrpos($className, '\\')) {
+            $namespace = substr($className, 0, $lastNsPos);
+            $className = substr($className, $lastNsPos + 1);
+            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+        require __DIR__ . '/../' . $fileName;
+    }
+}
+
+Autoloader::register();
