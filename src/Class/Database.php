e4bd7c41fd7a84d7a05ed2b521a0f65d20e90ee0
gratbrav <spam@huxsohl.de>
2016-11-30 19:54:25 +0100

    format code

--- src/Class/Database.php
+++ src/Class/Database.php
@@ -1,71 +1,65 @@
 <?php
-
-include_once __DIR__ . '/../db.php';
-	
 /**
-* Database Class
-*
-* Class for database connectiony
-*
-* @package  gratbrav
-* @author   Gratbrav
-* @version  $Revision: 1.0 $
-* @access   public
-*/
+ * Database Class
+ *
+ * Class for database connectiony
+ *
+ * @package  Torrentbug
+ * @author   Gratbrav
+ */
+include_once __DIR__ . '/../db.php';
+
 class Class_Database
 {
-	/**
-	 * Database
-	 */
-	protected $db = '';
+    /**
+     * Database
+     */
+    protected $db = '';
 
-	/**
-	 * Instance
-	 * 
-	 * @var self
-	 */
-	static private $instance = null;
+    /**
+     * Instance
+     * 
+     * @var self
+     */
+    static private $instance = null;
 
-	static public function getInstance()
-	{
-		if (null === self::$instance) {
-			self::$instance = new self;
-		}
-		return self::$instance;
-	}
+    static public function getInstance()
+    {
+        if (null === self::$instance) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
 
+    /**
+     * Get Database
+     */
+    public function getDatabase()
+    {
+        return $this->db;
+    }
 
-	/**
-	 * Get Database
-	 */
-	public function getDatabase()
-	{
-		return $this->db;
-	}
+    /**
+     * Constructor from Database Class
+     */
+    private function __construct()
+    {
+        include(__DIR__ . '/../config.php');
+        include(__DIR__ . '/../adodb/adodb.inc.php');
 
+        // 2004-12-09 PFM: connect to database.
+        $db = NewADOConnection($cfg['db_type']);
+        $db->Connect($cfg['db_host'], $cfg['db_user'], $cfg['db_pass'], $cfg['db_name']);
 
-	/**
-	 * Constructor from Database Class
-	 */
-	private function __construct()
-	{
-		include(__DIR__ . '/../config.php');
-		include(__DIR__ . '/../adodb/adodb.inc.php');
-		
-		// 2004-12-09 PFM: connect to database.
-		$db = NewADOConnection($cfg['db_type']);
-		$db->Connect($cfg['db_host'], $cfg['db_user'], $cfg['db_pass'], $cfg['db_name']);
-		
-		if (!$db) {
-        	die ('Could not connect to database: '.$db->ErrorMsg().'<br>Check your database settings in the config.php file.');
-    	}
-    
-    	$this->db = $db;
-	}
+        if (!$db) {
+            die ('Could not connect to database: '.$db->ErrorMsg().'<br>Check your database settings in the config.php file.');
+        }
 
+        $this->db = $db;
+    }
 
-	/**
-	 * disable clone of class
-	 */
-	private function __clone(){}
+    /**
+     * disable clone of class
+     */
+    private function __clone(){}
 }
