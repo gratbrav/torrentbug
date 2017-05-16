f5a85666c848d08e16228e1b3964199877100595
gratbrav <spam@huxsohl.de>
2017-05-16 18:23:24 +0200

    update history list

--- src/history.php
+++ src/history.php
@@ -25,72 +25,71 @@ include_once 'Class/autoload.php';
 include_once 'config.php';
 include_once 'functions.php';
 
-$settings = new Gratbrav\Torrentbug\Settings();
+    $settings = new Gratbrav\Torrentbug\Settings();
 
-include_once 'header.php';
+    include_once 'header.php';
 
+    $filter = [
+        'action' => [$cfg["constants"]["url_upload"], $cfg["constants"]["file_upload"]],
+    ];
+
+    $log = new \Gratbrav\Torrentbug\Log\Service();
+    $log->setFilter($filter);
 ?>
-<script
-    src="<?=$settings->get('base_url')?>/plugins/datatables/datatables/media/js/jquery.dataTables.min.js"></script>
-<script
-    src="<?=$settings->get('base_url')?>/plugins/datatables/datatables/media/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= $settings->get('base_url') ?>/plugins/datatables/datatables/media/js/jquery.dataTables.min.js"></script>
+<script src="<?= $settings->get('base_url') ?>/plugins/datatables/datatables/media/js/dataTables.bootstrap4.min.js"></script>
 
-<link
-    href="<?=$settings->get('base_url')?>/plugins/datatables/datatables/media/css/dataTables.bootstrap4.min.css"
-    type="text/css" rel="stylesheet" />
-<link
-    href="<?=$settings->get('base_url')?>/plugins/twitter/bootstrap/dist/css/bootstrap.min.css"
-    type="text/css" rel="stylesheet" />
+<link href="<?= $settings->get('base_url') ?>/plugins/datatables/datatables/media/css/dataTables.bootstrap4.min.css" type="text/css" rel="stylesheet" />
+<link href="<?= $settings->get('base_url') ?>/plugins/twitter/bootstrap/dist/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
 
 <div class="container">
     <div class="row">
-        <div class="col-sm-12 bd-example" style="padding: 6px">
 
-            <table id="historylist"
-                class="table table-striped table-bordered">
+        <div class="card">
+            <div class="card-header">
+                <i class="fa fa-users" aria-hidden="true"></i>
+                <?= _USERDETAILS ?>
+            </div>
+            <div class="card-block">
+
+                <table id="historylist" class="table table-striped table-bordered">
                 <thead>
                     <tr>
-                        <th><?php echo _USER; ?></th>
-                        <th><?php echo _FILE; ?></th>
-                        <th><?php echo _TIMESTAMP; ?></th>
+                        <th><?= _USER ?></th>
+                        <th><?= _FILE ?></th>
+                        <th><?= _TIMESTAMP ?></th>
                     </tr>
                 </thead>
-                <tbody>     		
-    			<?php
-    $sql = "SELECT user_id, file, time FROM tf_log WHERE action=" . $db->qstr($cfg["constants"]["url_upload"]) . " OR action=" . $db->qstr($cfg["constants"]["file_upload"]) . " ORDER BY time desc";
-    
-    $result = $db->SelectLimit($sql);
-    while (list ($user_id, $file, $time) = $result->FetchRow()) {
-        $iconColor = "#D9534F";
-        if (IsOnline($user_id)) {
-            $iconColor = '#5CB85C';
-        }
-        ?>
-        			<tr>
-                        <td><a
-                            href="message.php?to_user=<?php echo $user_id; ?>"><i class="fa fa-user" aria-hidden="true" style="color:<?php echo $iconColor; ?>;margin-right:4px;"></i><?php echo $user_id; ?></a>&nbsp;&nbsp;</td>
-                        <td><?php echo $file; ?></td>
-                        <td style="text-align: center;"><?php echo date(_DATETIMEFORMAT, $time); ?></td>
-                    </tr>
-            		<?php }	?>
+                <tbody>
+                    <?php foreach ($log->getLogs() as $log) { ?>
+                        <?php $iconClass = (IsOnline($log->getUserId())) ? 'text-success' : 'text-muted'; ?>
+                        <tr>
+                            <td>
+                                <a href="message.php?to_user=<?= $user_id; ?>">
+                                    <i class="fa fa-user <?= $iconClass ?>" aria-hidden="true"></i>
+                                    <?= $log->getUserId() ?>
+                                </a>
+                           </td>
+                           <td><?= $log->getFile() ?></td>
+                           <td><?= date(_DATETIMEFORMAT, $log->getTime()); ?></td>
+                       </tr>
+                    <?php } ?>
                 </tbody>
-            </table>
-
+                </table>
+            </div>
         </div>
+
     </div>
 </div>
 
-<link rel="stylesheet"
-    href="/src/plugins/datatables/datatables/media/css/dataTables.bootstrap4.min.css" />
-<script
-    src="/src/plugins/datatables/datatables/media/js/jquery.dataTables.min.js"></script>
-<script
-    src="/src/plugins/datatables/datatables/media/js/dataTables.bootstrap4.min.js"></script>
+<link rel="stylesheet" href="/src/plugins/datatables/datatables/media/css/dataTables.bootstrap4.min.css" />
+<script src="/src/plugins/datatables/datatables/media/js/jquery.dataTables.min.js"></script>
+<script src="/src/plugins/datatables/datatables/media/js/dataTables.bootstrap4.min.js"></script>
 
 <script>
-$(document).ready(function() {
-    $('#historylist').dataTable( {
+$( document ).ready( function() {
+    $( "#historylist" ).dataTable( {
         lengthChange: false,
-    });
-});
+    } );
+} );
 </script>
